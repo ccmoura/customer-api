d8c96304378d97d7e633f7acf6d7baf6a1488a48
christophmoura <christopher.moura@tiba.io>
2023-06-11 21:19:42 -0300

    feat: add env validation on app module

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const port = process.env.PORT;
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(port, () => {
+    console.log(`Server running at port ${port}.`);
+  });
 }
 bootstrap();
